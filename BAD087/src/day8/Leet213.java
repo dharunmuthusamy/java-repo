26a8f58ef89c82dbd525bdb687ca501ee707b180
dharunmuthusamy <dharunshivani30@gmail.com>
2026-01-27 22:06:11 +0530

    day8-revised

--- BAD087/src/day8/Leet213.java
+++ BAD087/src/day8/Leet213.java
@@ -1,14 +1,16 @@
 package day8;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Leet213 {
 	public static int sol(int[] arr,int l,int r) {
-		int[] dp = new int[arr.length-1];
+		int prev1 = 0,prev2=0;
 		for(int i=l;i<=r;i++) {
-			dp[i] = Math.max(dp[i-1], arr[i]+dp[i-2]);
+			int curr = Math.max(prev1, prev2+arr[i]);
+			prev2 = prev1;
+			prev1 = curr;
 		}
-		return dp[dp.length-1];
+		return prev1;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
